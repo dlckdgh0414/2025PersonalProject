ac8cb5054aab6cb667c6e44e912b8d493a496187
dlckdgh0414 <ci5976547@gmail.com>
2025-06-01 23:17:23 +0900

    feat : Stage2

--- Assets/02.Scripts/Gimmick/DisposableRoad.cs
+++ Assets/02.Scripts/Gimmick/DisposableRoad.cs
@@ -1,11 +1,20 @@
+using Unity.AI.Navigation;
 using UnityEngine;
 
 public class DisposableRoad : MonoBehaviour
 {
+    [SerializeField] private NavMeshSurface navMeshSurface;
+
+    private void OnDestroy()
+    {
+        navMeshSurface.BuildNavMesh();
+    }
+
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            
             Destroy(gameObject);
         }
     }
